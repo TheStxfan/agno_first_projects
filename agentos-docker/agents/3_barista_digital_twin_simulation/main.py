ba3f8b3666a1c597a4b41039e806a9f57cfb7355
TheStxfan <stefan.ferro5@gmail.com>
2026-06-25 13:56:24 +0200

    feat: add script to export database to JSONL

--- agentos-docker/agents/3_barista_digital_twin_simulation/main.py
+++ agentos-docker/agents/3_barista_digital_twin_simulation/main.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from service.simulation_service import run_continuous
 from dao.session_dao import get_all_sessions, init_custom_db
+from export_dataset import export_db_to_jsonl
 
 env_path = Path(__file__).resolve().parent.parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
@@ -12,6 +13,7 @@ def main():
     # Modifica num_sessions per quante conversazioni vuoi generare
     run_continuous(num_sessions=5)
     # print(get_all_sessions())
+    export_db_to_jsonl()
 
 
 if __name__ == "__main__":
