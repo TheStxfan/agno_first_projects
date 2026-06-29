25742914adbe201c016017ab41cbda1cf16cee7c
TheStxfan <stefan.ferro5@gmail.com>
2026-06-29 17:20:55 +0200

    feat: Update docker configuration for model fine tuning

--- windows_docker_recompose.bat
+++ windows_docker_recompose.bat
@@ -8,11 +8,16 @@ cd agentos-docker
 
 @REM docker compose down --remove-orphans
 @REM docker compose up -d --force-recreate --build
-@REM docker compose down
-@REM docker compose build --no-cache
-@REM docker compose up -d
-
 docker compose down
+@REM docker compose build --no-cache
 docker compose up
+
+@REM docker build --no-cache --progress=plain -t barista-twin .
+@REM docker run --gpus all -it barista-twin
+
+@REM docker compose down
+@REM docker compose up
+
+@REM docker container prune
 
 pause
